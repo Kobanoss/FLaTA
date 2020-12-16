3cb48dcefa91f184b5b51d1aa5be252a849a8aa2
Kobanoss <47609905+Kobanoss@users.noreply.github.com>
2020-12-17 02:45:40 +0300

    Update main.c

--- graph_flata/main.c
+++ graph_flata/main.c
@@ -5,7 +5,16 @@
 
 
 #define UNUSED(argc) (void)(argc)
-typedef enum {False, True} bool;
+typedef enum {
+    False, True
+} bool;
+
+void swap(int *x_ptr, int *y_ptr) {
+    int tmp = *x_ptr;
+    *x_ptr = *y_ptr;
+    *y_ptr = tmp;
+}
+
 int min_p;
 
 
@@ -19,6 +28,7 @@ typedef struct Graph {
     int size;
     bool is_directed;
     bool *visited;
+    int *degree;
     node **points;
 } Graph;
 
@@ -35,13 +45,15 @@ Graph *createGraph(int size, bool is_directed) {
     Graph *graph = malloc(sizeof(Graph));
     graph->size = size;
     graph->is_directed = is_directed;
-    graph->points = malloc(size * sizeof(node*));
+    graph->points = malloc(size * sizeof(node *));
     graph->visited = malloc(size * sizeof(bool));
+    graph->degree = malloc(size * sizeof(int));
 
 
     for (int i = 0; i < size; i++) {
         graph->points[i] = NULL;
         graph->visited[i] = False;
+        graph->degree[i] = 0;
     }
     return graph;
 }
@@ -63,8 +75,7 @@ void addEdge(Graph *graph, int first_p, int second_p) {
 }
 
 void DFS(Graph *graph, int start) {
-    node *p_list = graph->points[start];
-    node *tmp = p_list;
+    node *tmp = graph->points[start];
 
     graph->visited[start] = True;
     printf("  Visited %d \n", start);
@@ -78,11 +89,12 @@ void DFS(Graph *graph, int start) {
         tmp = tmp->next;
     }
 }
+
 void check(Graph *graph) {
-    int counter = 0,counter_v = 0;
+    int counter = 0, counter_v = 0;
 
     for (int i = 0; i < graph->size; i++) {
-        if ((graph->visited[i] == True ) || (graph->points[i])) counter++;
+        if ((graph->visited[i] == True) || (graph->points[i])) counter++;
     }
 
     for (int i = 0; i < graph->size; i++) {
@@ -106,8 +118,8 @@ int fromFile(FILE *input_file, Graph *graph, bool get_size) {
         fseek(input_file, -1, SEEK_CUR);
 
         int step = ftell(input_file);
-        if (((graph->is_directed == False) && fscanf(input_file, "%d -- %d",&_, &_))||
-            ((graph->is_directed == True) && fscanf(input_file, "%d -> %d",&_,&_))) {
+        if (((graph->is_directed == False) && fscanf(input_file, "%d -- %d", &_, &_)) ||
+            ((graph->is_directed == True) && fscanf(input_file, "%d -> %d", &_, &_))) {
             fseek(input_file, step, SEEK_SET);
 
             int first_p, second_p;
@@ -119,14 +131,15 @@ int fromFile(FILE *input_file, Graph *graph, bool get_size) {
             if (max_p < first_p) max_p = first_p;
             if (max_p < second_p) max_p = second_p;
             if (get_size == False) addEdge(graph, first_p, second_p);
+        } else {
+            printf("Troubles inside input file");
+            exit(130);
         }
-        else {printf("Troubles inside input file"); exit(130);}
 
     }
     if (get_size == False) {
         return 0;
-    }
-    else return max_p;
+    } else return max_p;
 }
 
 void toDot(FILE *output_file, Graph *graph) {
@@ -135,28 +148,32 @@ void toDot(FILE *output_file, Graph *graph) {
 
     for (int v = 0; v < graph->size; v++) {
         node *tmp = graph->points[v];
-        while (tmp && tmp->uniq == True) {
-
-            if (graph->is_directed == True) fprintf(output_file, "%d->%d;", v, tmp->vertex);
-            else fprintf(output_file, "%d--%d;", v, tmp->vertex);
+        while (tmp) {
+            if (v == tmp->vertex && tmp->uniq == False) {
+                tmp = tmp->next;
+                continue;
+            } else if (tmp->uniq == True) {
+                if (graph->is_directed == True) fprintf(output_file, "%d->%d;", v, tmp->vertex);
+                else fprintf(output_file, "%d--%d;", v, tmp->vertex);
+            }
             tmp = tmp->next;
         }
     }
     fputc('}', output_file);
 }
-void toPNG(FILE* input_file, char* output) {
+
+void toPNG(FILE *input_file, char *output) {
     char operation[256];
     char dotFile[128];
 
     fseek(input_file, 0, SEEK_SET);
-    fread(dotFile,1,128,input_file);
+    fread(dotFile, 1, 128, input_file);
 
     snprintf(operation, sizeof(operation), "echo \"%s\" | dot -Tpng >%s", dotFile, output);
     system(operation);
 
-
-
 }
+
 void findLoop(Graph *graph) {
     for (int v = 0; v < graph->size; v++) {
         node *tmp = graph->points[v];
@@ -165,10 +182,62 @@ void findLoop(Graph *graph) {
             if (v == tmp->vertex && tmp->uniq == True) n++;
             tmp = tmp->next;
         }
-        if (n!=0) printf("Vertex %d is looped %d times\n", v, n);
+        if (n != 0) printf("Vertex %d is looped %d times\n", v, n);
     }
     printf("\n");
 
+}
+
+void findDegree(Graph *graph) {
+    for (int v = 0; v < graph->size; v++) {
+        node *tmp = graph->points[v];
+        int n = 0;
+        while (tmp) {
+            n++;
+            tmp = tmp->next;
+        }
+        if (n != 0) printf("Vertex %d has %d degree\n", v, n);
+        graph->degree[v] = n;
+    }
+    printf("\n");
+}
+
+void quickSort(int *arr, int first, int last) {
+    if (first < last) {
+        int left = first, right = last, middle = arr[(left + right) / 2];
+        do {
+            while (arr[left] < middle) left++;
+            while (arr[right] > middle) right--;
+            if (left <= right) {
+                swap(&arr[left], &arr[right]);
+                left++;
+                right--;
+            }
+        } while (left <= right);
+        quickSort(arr, first, right);
+        quickSort(arr, left, last);
+    }
+}
+
+void degreeSort(Graph *graph) {
+    int *degrees = malloc(sizeof(graph->degree));
+    for (int v = 0; v < graph->size; v++) degrees[v] = graph->degree[v];
+    int points[graph->size];
+    for (int i = 0; i < graph->size; i++) points[i] = i;
+
+    quickSort(degrees, 0, graph->size - 1);
+
+    for (int v = 0; v < graph->size; v++) {
+        for (int i = 0; i < graph->size; i++) {
+            if (degrees[v] == graph->degree[points[i]]) {
+                if (degrees[v]!=0 && points[i]!=0) {
+                    points[i] = -1;
+                    printf("Vertex%d with %d degree\n", i, degrees[v]);
+                }
+            }
+        }
+    }
+    printf("\n");
 }
 
 void printGraph(Graph *graph) {
@@ -176,6 +245,10 @@ void printGraph(Graph *graph) {
         node *tmp = graph->points[v];
         if (tmp) printf("\nVertex %d:\n", v);
         while (tmp) {
+            if (v == tmp->vertex && tmp->uniq == False) {
+                tmp = tmp->next;
+                continue;
+            }
             printf("Connected to: %d \n", tmp->vertex);
             tmp = tmp->next;
         }
@@ -184,12 +257,11 @@ void printGraph(Graph *graph) {
 }
 
 void deleteGraph(Graph *graph) {
-    for (int i = 0; i<=graph->size-1; i++) {
+    for (int i = 0; i <= graph->size - 1; i++) {
         free(graph->points[i]);
     }
     free(graph->visited);
     free(graph);
-
 }
 
 
@@ -199,22 +271,35 @@ int main(int argc, char *argv[]) {
 
     FILE *input_file = fopen(argv[1], "r");
     FILE *output_file = fopen(argv[2], "w+");
-    if (!input_file) {printf("Troubles with opening input file"); exit(130);}
-    if (!output_file) {printf("Troubles with opening output file"); exit(130);}
+    if (!input_file) {
+        printf("Troubles with opening input file");
+        exit(130);
+    }
+    if (!output_file) {
+        printf("Troubles with opening output file");
+        exit(130);
+    }
 
     if (!strcmp(argv[4], "-directed")) is_directed = True;
     else if (!strcmp(argv[4], "-undirected"))is_directed = False;
-    else {printf("Invalid arguments"); exit(130);}
+    else {
+        printf("Invalid arguments");
+        exit(130);
+    }
 
-    Graph *get_size = createGraph(-1,is_directed);
+    Graph *get_size = createGraph(-1, is_directed);
     int size = fromFile(input_file, get_size, True);
     deleteGraph(get_size);
-    Graph *graph = createGraph(size+1, is_directed);
+    Graph *graph = createGraph(size + 1, is_directed);
 
     fromFile(input_file, graph, False);
     toDot(output_file, graph);
     printGraph(graph);
     findLoop(graph);
+    printf("Vertex degrees:\n");
+    findDegree(graph);
+    printf("Quick sorted degree's array:\n");
+    degreeSort(graph);
 
     printf("DFS start...\n");
     DFS(graph, min_p);
@@ -224,7 +309,7 @@ int main(int argc, char *argv[]) {
     check(graph);
 
     printf("\nPrinting graph to png...\n");
-    toPNG(output_file,argv[3]);
+    toPNG(output_file, argv[3]);
     printf("\nGraph printed, please check %s\n", argv[3]);
 
     fclose(output_file);
